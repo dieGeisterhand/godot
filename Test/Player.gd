2cdff9aaaa09a5df307fa6d8b4b0fca69fd3d0eb
dieGeisterhand <jeyfrem@proton.me>
2024-08-05 19:15:22 -0400

    initial commit

---
+++ Test/Player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+func _physics_process(delta):
+	var move_speed : float = 200.0
+	velocity.x = 0
+	velocity.y = 0
+	
+	if Input.is_key_pressed(KEY_LEFT):
+		velocity.x -= 1
+	if Input.is_key_pressed(KEY_RIGHT):
+		velocity.x += 1
+	if Input.is_key_pressed(KEY_UP):
+		velocity.y -= 1
+	if Input.is_key_pressed(KEY_DOWN):
+		velocity.y += 1
+	
+	velocity *= move_speed
+		
+	move_and_slide()
